428492c19d63fa5270810fca9039f1a109d1f7db
Maisha Maliha <maishamaliha@maishas-macbook-air.local>
2019-01-30 22:56:53 -0800

    Initial Commit

---
+++ tipcalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipcalc
+//
+//  Created by Maisha Maliha on 1/30/19.
+//  Copyright © 2019 Maisha Maliha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
